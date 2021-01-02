0610313626c8024bd68f27c8d1e99f98cff3a779
CASL0 <gin.0046.822@gmail.com>
2021-01-02 17:13:26 +0900

    エラーハンドリング用クラスの作成
    * エラーハンドリングを例外に一貫させるため
    * Win32のGetLastError()を例外に変換するクラス

---
+++ LibFirewall/Win32Exception.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <Windows.h>
+#include <string>
+#include <vector>
+#include <stdexcept>
+
+namespace Win32Util 
+{
+    std::string FormatErrorMessage(DWORD error, const std::string& msg)
+    {
+        static const int BUFFERLENGTH = 1024;
+        std::vector<char> buf(BUFFERLENGTH);
+        FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM, 0, error, 0, buf.data(), BUFFERLENGTH - 1, 0);
+        return std::string(buf.data()) + "   (" + msg + ")";
+    }
+
+    class CWin32Exception : public std::runtime_error
+    {
+    private:
+        DWORD m_dwError;
+    public:
+        CWin32Exception(DWORD error, const std::string& msg)
+            : runtime_error(FormatErrorMessage(error, msg)), m_dwError(error) { }
+
+        DWORD GetErrorCode() const { return m_dwError; }
+    };
+
+    void ThrowWin32Error(bool expression, const std::string& msg)
+    {
+        if (expression) 
+        {
+            throw CWin32Exception(GetLastError(), msg);
+        }
+    }
+}
+
