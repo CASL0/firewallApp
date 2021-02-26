987fb72a4dea0a82bdf8685b51ab20395b2d27a4
CASL0 <gin.0046.822@gmail.com>
2021-02-26 22:26:52 +0900

    文字列リソースをStringTableに集約

--- firewall/resource.h
+++ firewall/resource.h
@@ -3,6 +3,24 @@
 // firewall.rc ‚ÅŽg—p
 //
 #define IDD_DIALOG1                     101
+#define IDS_APP_TITLE                   103
+#define IDS_ERROR_FW_INIT               104
+#define IDS_ERROR_FW_ADD_FILTER         105
+#define IDS_ERROR_FW_RM_FILTER          106
+#define IDS_CONFIRM_FW_RM_FILTER        107
+#define IDS_BTN_LABEL_ADD               108
+#define IDS_BTN_LABEL_DEL               109
+#define IDS_BTN_LABEL_ALLBLOCK_ENABLE   110
+#define IDS_BTN_LABEL_ALLBLOCK_DISABLE  111
+#define IDS_STATIC_TEXT_IPADDR          112
+#define IDS_STATIC_TEXT_PORT            113
+#define IDS_STATIC_TEXT_FQDN            114
+#define IDS_STATIC_TEXT_PROTOCOL        115
+#define IDS_STATIC_TEXT_URL             116
+#define IDS_STATIC_TEXT_PROCESS         117
+#define IDS_STATIC_TEXT_ACTION          118
+#define IDS_STATIC_TEXT_ALLBLOCK        119
+#define IDS_STATIC_TEXT_ALLBLOCK_ENABLE 119
 #define IDC_BUTTON_ADD                  1001
 #define IDC_IPADDRESS                   1002
 #define IDC_EDIT_PROTOCOL               1003
@@ -27,7 +45,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1026
 #define _APS_NEXT_SYMED_VALUE           101
